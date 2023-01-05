c9a1a33b0c36f208da396656ed5dce2ede34df46
Roman Permiakov <info@presstomsk.ru>
2023-01-05 20:53:37 +0700

    упаковка и минификация кода

---
+++ Web_Parent_Control/wwwroot/js/bundle.js
@@ -0,0 +1,41 @@
+let container = document.querySelector('.dataTable');
+
+container.addEventListener('click', function () {    
+    if (event.target.className == 'btn-false') {       
+        const request = new XMLHttpRequest(); 
+        let data = "site=" + event.target.title;
+        request.open("POST", `/block`);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.send(data);
+        let items = document.querySelectorAll('.btn-false');
+        console.log(items.length);
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].title == event.target.title)
+            {
+                items[i].classList.add('btn-true');
+                items[i].classList.remove('btn-false');
+                items[i].value = "Разблокировать сайт";
+                items[i].style = "background: lightgreen;";
+            }
+        }
+        return;
+    }
+    else if (event.target.className == 'btn-true')
+    {
+        const request = new XMLHttpRequest();
+        let data = "site=" + event.target.title;
+        request.open("POST", `/unblock`);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.send(data);
+        let items = document.querySelectorAll('.btn-true');
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].title == event.target.title) {
+                items[i].classList.add('btn-false');
+                items[i].classList.remove('btn-true');
+                items[i].value = "Блокировать сайт";
+                items[i].style = "background: lightcoral;";
+            }
+        }           
+    }
+})
+
